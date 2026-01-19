ec40c17b27bfe196d365edc735eeca4d4ebbef2c
Vishal Kumar Singh <vishalkumar70854@gmail.com>
2026-01-19 14:14:55 +0530

    2 way data binding using ngModel

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,13 +3,16 @@ import { RouterOutlet } from '@angular/router';
 import { MyComponent } from './my-component/my-component';
 import { User } from './user/user';
 import { Product } from './product/product';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MyComponent, User, Product],
+  imports: [RouterOutlet, MyComponent, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   title = 'Vishal Kumar Singh';
+
+  message: any = "my message";
 }
